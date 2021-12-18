d911e2a3b2edb743929994515475d012742acc2a
Partha Singha Roy <parthasingharoy166@gmail.com>
2021-12-18 20:47:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const connectToMongo = require('./Database/db')
+
+const app = express()
+const PORT = 5000
+connectToMongo()
+
+app.get('/',(req,res)=>{
+   res.send('Hello World!')
+})
+
+app.listen(PORT, ()=>{console.log(`Server is running at port &{PORT}`)})
